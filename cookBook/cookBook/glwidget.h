842d8000ce0d183f76b5d36cd5c51923db72c880
brunoferraz <brunoferraz.pro@gmail.com>
2015-09-21 18:54:02 -0300

    lab to home - Cookbook simple example is ok

--- cookBook/cookBook/glwidget.h
+++ cookBook/cookBook/glwidget.h
@@ -2,7 +2,15 @@
 #define GLWIDGET_H
 
 #include <QWidget>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include <QGLWidget>
+#include <QDebug>
 
 
 class GLWidget : public QGLWidget
@@ -10,6 +18,16 @@ class GLWidget : public QGLWidget
     Q_OBJECT
 public:
     explicit GLWidget(QWidget *parent = 0);
+
+    const char *loadShaderAsString(const char* file);
+
+    GLuint prepareShader(std::string path, GLenum type);
+    void readAttributes();
+
+    GLuint programHandle;
+    GLuint vaoHandle;
+
+    void initializeGL();
     void paintGL();
 
 signals:
--- cookBook/cookBook/main.cpp
+++ cookBook/cookBook/main.cpp
@@ -333,21 +333,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    GLenum err = glewInit();
-    if(GLEW_OK != err){
-        std::cout << glewGetErrorString(err) << std::endl;
-    }else{
-        std::cout << "GLEW OK" << std::endl;
-        const GLubyte *renderer = glGetString(GL_RENDERER);
-        const GLubyte *vendor   = glGetString(GL_VENDOR);
-        const GLubyte *version  = glGetString(GL_VERSION);
-        const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
-
-        std::cout << renderer << " | " << vendor << std::endl;
-        std::cout << "OPENGL  |  version: " << version << std::endl;
-        std::cout << "GLSL    |  version: " << glslVersion << std::endl;
-
-        initScene();
-    }
     return a.exec();
 }
